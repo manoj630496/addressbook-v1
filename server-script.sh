7a246e464cfa82bc0b0fc1a1a21f9e11b637cd2b
Manoj Kumar Koppineedi <koppineedimanojkumar@outlook.com>
2025-03-05 22:49:19 +0530

    script updated

--- server-script.sh
+++ server-script.sh
@@ -1,13 +1,12 @@
-sudo yum install java-1.8.0-openjdk-devel -y
-#sudo yum install java -y
 sudo yum install git -y
-sudo yum install maven -y
+sudo yum install docker -y
+sudo systemctl start docker
 
 if [ -d "addressbook-v1" ]
 then
   echo "repo is cloned and exists"
   cd /home/ec2-user/addressbook-v1
-  git pull origin demo1
+  git pull origin b3
 else
   git clone https://github.com/manoj630496/addressbook-v1.git
 fi
